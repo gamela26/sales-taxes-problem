8f8a7d3142258cb19c208e8021d769802fcc0b12
Daniel Cantoni <daniel.cantoni90@gmail.com>
2017-02-05 17:17:51 +0100

    Implemented tax and price test

--- src/test/java/me/daniel/sales_taxes_problem/OrderItemTest.java
+++ src/test/java/me/daniel/sales_taxes_problem/OrderItemTest.java
@@ -10,10 +10,18 @@ import org.junit.Test;
 public class OrderItemTest {
 
 	private OrderItem orderItem;
+	private SalesTaxStrategy taxStrategy;
 	
 	@Before
 	public void setUp(){
 		orderItem = new OrderItem(1, new Product("chocolate", ProductType.FOOD, false, new BigDecimal(12.34)));
+		taxStrategy = new BaseSalesTax();
+	}
+	
+	@Test 
+	public void testGetters(){
+		assertEquals(new Integer(1), orderItem.getQuantity());
+		assertEquals(new Product("chocolate", ProductType.FOOD, false, new BigDecimal(12.34)), orderItem.getProduct());
 	}
 	
 	@Test
@@ -31,6 +39,42 @@ public class OrderItemTest {
 		assertTrue(orderItem.equals(same));
 		assertFalse(orderItem.equals(diff1));
 		assertFalse(orderItem.equals(diff2));
+	}
+	
+	@Test
+	public void testGetTotalTax(){
+		OrderItem orderItem = new OrderItem (1, new Product("other", ProductType.OTHER, true, new BigDecimal(10.00)));
+		BigDecimal expectedTax = new BigDecimal(1.5).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		OrderItem orderItem2 = new OrderItem (2, new Product("other", ProductType.OTHER, true, new BigDecimal(10.00)));
+		BigDecimal expectedTax2 = new BigDecimal(3).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		assertEquals(expectedTax, orderItem.getTotalTax(taxStrategy));
+		assertEquals(expectedTax2, orderItem2.getTotalTax(taxStrategy));
+	}
+	
+	@Test
+	public void testGetTotalNetPrice(){
+		OrderItem orderItem = new OrderItem (1, new Product("other", ProductType.OTHER, true, new BigDecimal(10.00)));
+		BigDecimal expectedNetPrice = new BigDecimal(10).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		OrderItem orderItem2 = new OrderItem (2, new Product("other", ProductType.OTHER, true, new BigDecimal(10.00)));
+		BigDecimal expectedNetPrice2 = new BigDecimal(20).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		assertEquals(expectedNetPrice, orderItem.getTotalNetPrice());
+		assertEquals(expectedNetPrice2, orderItem2.getTotalNetPrice());
+	}
+	
+	@Test
+	public void testGetTotalPrice(){
+		OrderItem orderItem = new OrderItem (1, new Product("other", ProductType.OTHER, true, new BigDecimal(10.00)));
+		BigDecimal expectedPrice = new BigDecimal(11.5).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		OrderItem orderItem2 = new OrderItem (2, new Product("other", ProductType.OTHER, true, new BigDecimal(10.00)));
+		BigDecimal expectedPrice2 = new BigDecimal(23).setScale(2, BigDecimal.ROUND_HALF_UP);
+		
+		assertEquals(expectedPrice, orderItem.getTotalPrice(taxStrategy));
+		assertEquals(expectedPrice2, orderItem2.getTotalPrice(taxStrategy));
 	}
 
 }
